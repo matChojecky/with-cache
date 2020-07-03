5cdffe8dc4f3c73b2571f255dec54370652dacee
matChojecky <mchojecky@gmail.com>
2020-07-03 10:22:13 +0200

    Fix importing issues

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,4 +4,3 @@ export interface WithCacheOptions<T extends (...args: any) => any> {
 }
 export declare type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
 export default function withCache<F extends (...args: unknown[]) => unknown>(fn: F, opts?: WithCacheOptions<typeof fn>): (...args: Parameters<typeof fn>) => Promise<UnwrapPromise<ReturnType<typeof fn>>>;
-//# sourceMappingURL=index.d.ts.map
