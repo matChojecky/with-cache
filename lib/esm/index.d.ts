6bf49c88166afeab1bc94ff76310994720dd9b5c
matChojecky <mchojecky@gmail.com>
2020-07-03 11:19:54 +0200

    use named export

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -3,4 +3,4 @@ export interface WithCacheOptions<T extends (...args: any) => any> {
     keyGen?: (...args: Parameters<T>) => string;
 }
 export declare type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
-export default function withCache<F extends (...args: unknown[]) => unknown>(fn: F, opts?: WithCacheOptions<typeof fn>): (...args: Parameters<typeof fn>) => Promise<UnwrapPromise<ReturnType<typeof fn>>>;
+export declare function withCache<F extends (...args: unknown[]) => unknown>(fn: F, opts?: WithCacheOptions<typeof fn>): (...args: Parameters<typeof fn>) => Promise<UnwrapPromise<ReturnType<typeof fn>>>;
