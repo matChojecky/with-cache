e16c87f1713f3db4e45d50dd4afd64a2e2efd901
matChojecky <mchojecky@gmail.com>
2020-07-03 10:58:30 +0200

    Change lib folder structure

---
+++ lib/cjs/index.d.ts
@@ -0,0 +1,6 @@
+export interface WithCacheOptions<T extends (...args: any) => any> {
+    validFor?: number;
+    keyGen?: (...args: Parameters<T>) => string;
+}
+export declare type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
+export default function withCache<F extends (...args: unknown[]) => unknown>(fn: F, opts?: WithCacheOptions<typeof fn>): (...args: Parameters<typeof fn>) => Promise<UnwrapPromise<ReturnType<typeof fn>>>;
