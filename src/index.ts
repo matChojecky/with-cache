21b0fa0211b5898ed2a3f60bd66767e08e1f6719
Mateusz Chojecki <mateusz.chojecki@MWPLKRAMACLT018.local>
2020-09-18 17:07:11 +0200

    Add docs setup

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,94 @@
-
-interface CacheOptions<Args extends unknown[]> {
+/**
+ * @typedef CacheOptions
+ * @typeParam Args Arguments array that base function accepts
+ * @typeParam CacheItem Type of returned value from base function 
+ */
+interface CacheOptions<Args extends unknown[], CacheItem extends unknown> {
+  /**
+   * Function used to calculate cache key
+   * @default (...args) => JSON.stringify(args)
+   * @returns returned value used as a cache key
+   */
   keymaker?: (...args: Args) => string | number;
+  /**
+   * Time to live of cached value in ms
+   * @default 3600000
+   */
+  ttl?: number;
+  /**
+   * Cache instance
+   * @default new Map()
+   */
+  cache?: Cache<CacheItem>;
 }
 
+/**
+ * @typeParam Args Arguments array that base function accepts
+ * @typeParam ResultType Type of returned value from base function 
+ */
 interface CacheResolver<Args extends unknown[], ResultType extends unknown> {
   (...args: Args): ResultType;
+  /**
+   * clear cache
+   */
+  clear(): void;
+  /**
+   * Using this method won't check for cached values and will straight call memoized fn and save new result to cache
+   */
+  refresh(...args: Args): ResultType;
 }
 
-type Cache<T> = Record<string | number, T>;
+type Cache<T> = Map<string | number, { value: T; validFor: number }>;
 
+/**
+ * @ignore
+ */
 const defaultKeymaker = <Args extends unknown[]>(...args: Args) =>
   JSON.stringify(args);
 
+/**
+ * Creates function that caches results returned from base function fn
+ * @typeParam Args Arguments array that base function accepts
+ * @typeParam ResultType Type of returned value from base function 
+ */
 export function withCache<Args extends unknown[], ResultType extends unknown>(
   fn: (...args: Args) => ResultType,
-  { keymaker = defaultKeymaker }: CacheOptions<Args> = {}
+  options: CacheOptions<Args, ResultType> = {}
 ): CacheResolver<Args, ResultType> {
-  const cache: Cache<ResultType> = {};
+  const {
+    keymaker = defaultKeymaker,
+    ttl = 3600000,
+    cache = new Map(),
+  } = options;
+
+  function runFuncAndSave(
+    key: ReturnType<typeof keymaker>,
+    ...args: Args
+  ): ResultType {
+    const result = fn(...args);
+    cache.set(key, { value: result, validFor: Date.now() + ttl });
+    return result;
+  }
 
   function cacheResolver(...args: Args) {
     const key = keymaker(...args);
-    const cachedValue = cache[key];
-    
-    if(!!cachedValue) {
-      return cachedValue;
+    const cachedValue = cache.get(key);
+
+    if (!!cachedValue && cachedValue.validFor > Date.now()) {
+      return cachedValue.value;
     }
 
-    const result = fn(...args);
-    cache[key] = result;
-    return result;
+    return runFuncAndSave(key, ...args);
   }
+
+  cacheResolver.clear = function () {
+    cache.clear();
+  };
+
+  cacheResolver.refresh = function (...args: Args): ResultType {
+    const key = keymaker(...args);
+    return runFuncAndSave(key, ...args);
+  };
 
   return cacheResolver;
 }
--- tests/index.ts
+++ tests/index.ts
@@ -1,17 +1,21 @@
 import assert from "assert";
 import { withCache } from "../src/index";
+import { sleep } from "./helpers";
 
 describe("withCache", () => {
   it("should return function", () => {
-    const cached = withCache(() => {});
+    const fn = () => {
+      return Date.now();
+    };
+    const cached = withCache(fn);
 
-    assert.equal(typeof cached, "function");
+    assert.strictEqual(typeof cached, "function");
   });
 
   it("should call function only once and then return cached value", () => {
     const fn = () => {
       fn.called += 1;
-      return Date.now(); 
+      return Date.now();
     };
 
     fn.called = 0;
@@ -21,56 +25,55 @@ describe("withCache", () => {
     resolver();
     const r2 = resolver();
 
-    assert.equal(fn.called, 1);
-    assert.equal(r1, r2);
+    assert.strictEqual(fn.called, 1);
+    assert.strictEqual(r1, r2);
   });
 
-  it('should accept args and return different cached values based on passed args', () => {
+  it("should accept args and return different cached values based on passed args", () => {
     const fn = (_: any) => {
       fn.called += 1;
-      return Date.now(); 
+      return Date.now();
     };
 
     fn.called = 0;
 
     const resolver = withCache(fn);
-    const result_1 = resolver('one');
-    const result_2 = resolver('two');
-    const result_1_cached = resolver('one');
-    const result_2_cached = resolver('two');
+    const result_1 = resolver("one");
+    const result_2 = resolver("two");
+    const result_1_cached = resolver("one");
+    const result_2_cached = resolver("two");
 
-    assert.equal(fn.called, 2);
-    assert.equal(result_1, result_1_cached);
-    assert.equal(result_2, result_2_cached);
-
+    assert.strictEqual(fn.called, 2);
+    assert.strictEqual(result_1, result_1_cached);
+    assert.strictEqual(result_2, result_2_cached);
   });
 
-  it('should accept custom keymaker returning constant key returning first call', () => {
+  it("should accept custom keymaker returning constant key returning first call", () => {
     const fn = (_: any) => {
       fn.called += 1;
-      return Date.now(); 
+      return Date.now();
     };
 
     fn.called = 0;
-    const keymaker = (_: any) => 'key';
-    const resolver = withCache(fn, {keymaker});
+    const keymaker = (_: any) => "key";
+    const resolver = withCache(fn, { keymaker });
     const result_1 = resolver(Math.random());
     const result_2 = resolver(Math.random());
 
-    assert.equal(fn.called, 1);
-    assert.equal(result_1, result_2);
+    assert.strictEqual(fn.called, 1);
+    assert.strictEqual(result_1, result_2);
   });
 
-  it('should accept custom keymaker resolving value based on created key', () => {
+  it("should accept custom keymaker resolving value based on created key", () => {
     const fn = (_label: any, _id: any) => {
       fn.called += 1;
-      return Date.now(); 
+      return Date.now();
     };
 
     fn.called = 0;
     const keymaker = (label: any, id: any) => `${label}_${id}`;
-    
-    const resolver = withCache(fn, {keymaker});
+
+    const resolver = withCache(fn, { keymaker });
     const id_1 = Math.random();
     const id_2 = Math.random();
 
@@ -79,29 +82,96 @@ describe("withCache", () => {
     const result_3 = resolver("uno", id_1);
     const result_4 = resolver("dos", id_2);
 
-    assert.equal(fn.called, 2);
-    assert.equal(result_1, result_3);
-    assert.equal(result_2, result_4);
+    assert.strictEqual(fn.called, 2);
+    assert.strictEqual(result_1, result_3);
+    assert.strictEqual(result_2, result_4);
   });
 
+  it("should call async function only once and then return cached value", () => {
+    const fn = async () => {
+      fn.called += 1;
+      return Date.now();
+    };
 
+    fn.called = 0;
 
-  //   it("should .purge method exist and reset cache", () => {
-  //     const fn = () => {
-  //       fn.called += 1;
-  //     };
+    const resolver = withCache(fn);
+    const r1 = resolver();
+    resolver();
+    const r2 = resolver();
 
-  //     fn.called = 0;
+    assert.strictEqual(fn.called, 1);
+    assert.strictEqual(r1, r2);
+  });
 
-  //     const cached = withCache(fn);
-  //     cached();
-  //     cached();
+  it("should have method to force calculation", () => {
+    const fn = () => {
+      return Date.now();
+    };
 
-  //     assert.equal(typeof cached.purge, "function");
+    const resolver = withCache(fn);
+    assert(typeof resolver.clear, "function");
+  });
 
-  //     cached.purge();
-  //     cached();
+  it("should recalculate result when cache is cleared", () => {
+    const fn = () => {
+      fn.called += 1;
+      return Date.now();
+    };
+    fn.called = 0;
 
-  //     assert.equal(fn.called, 2);
-  //   });
+    const resolver = withCache(fn);
+
+    resolver();
+    resolver.clear();
+    resolver();
+
+    assert.strictEqual(fn.called, 2);
+  });
+
+  it("should handle ttl and recalculate after valid date have passed", async function () {
+    const fn = () => {
+      fn.called += 1;
+      return Date.now();
+    };
+    fn.called = 0;
+
+    const resolver = withCache(fn, { ttl: 10 });
+
+    resolver();
+    resolver();
+    await sleep(11);
+    resolver();
+
+    assert.strictEqual(fn.called, 2);
+  });
+
+  it("should have refresh method", () => {
+    const fn = () => {
+      return Date.now();
+    };
+
+    const resolver = withCache(fn);
+
+    assert(typeof resolver.refresh, "function");
+  });
+
+  it("should call cached fn when used refresh", () => {
+    const fn = () => {
+      fn.called += 1;
+      return Date.now();
+    };
+    fn.called = 0;
+
+    const resolver = withCache(fn);
+
+    resolver();
+    resolver.refresh();
+    resolver();
+    resolver.refresh();
+    resolver();
+    resolver.refresh();
+
+    assert.strictEqual(fn.called, 4);
+  });
 });
